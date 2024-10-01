6b4a6736ea65efc48e01a90b517efffc51259d21
LAALAD <160270603+LAALAD@users.noreply.github.com>
2024-10-01 12:21:27 +0200

    v2

--- SOL/Practica2_2/src/practica2_2.java
+++ SOL/Practica2_2/src/practica2_2.java
@@ -28,59 +28,80 @@ public class practica2_2 extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel2 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        campo_texto = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        boton_saluda = new javax.swing.JButton();
+        campo_nombre = new javax.swing.JTextField();
+        icono_mano = new javax.swing.JLabel();
+        etiqueta_instruccion = new javax.swing.JLabel();
+        campo_apellido = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setLabel("Saluda");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        boton_saluda.setLabel("Saluda");
+        boton_saluda.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                boton_saludaActionPerformed(evt);
             }
         });
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/saluda.png"))); // NOI18N
+        campo_nombre.setForeground(new java.awt.Color(204, 204, 204));
+        campo_nombre.setText("Introduzca nombre");
+        campo_nombre.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                campo_nombreKeyPressed(evt);
+            }
+        });
 
-        jLabel2.setText("Escribe el nombre del usuario");
-        jLabel2.setAlignmentY(0.0F);
+        icono_mano.setIcon(new javax.swing.ImageIcon(getClass().getResource("/saluda.png"))); // NOI18N
+
+        etiqueta_instruccion.setText("Escribe el nombre del usuario");
+        etiqueta_instruccion.setAlignmentY(0.0F);
+
+        campo_apellido.setForeground(new java.awt.Color(204, 204, 204));
+        campo_apellido.setText("Introduzca apellido");
+        campo_apellido.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                campo_apellidoKeyPressed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap(56, Short.MAX_VALUE)
+                .addContainerGap(54, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(6, 6, 6)
-                        .addComponent(campo_texto, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                            .addComponent(jLabel1)
-                            .addGap(93, 93, 93))
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                            .addComponent(jLabel2)
-                            .addGap(42, 42, 42)))))
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(86, 86, 86)
-                .addComponent(jButton1)
-                .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                                .addComponent(icono_mano)
+                                .addGap(99, 99, 99))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                                .addComponent(etiqueta_instruccion)
+                                .addGap(44, 44, 44)))
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                            .addGap(6, 6, 6)
+                            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(campo_nombre, javax.swing.GroupLayout.DEFAULT_SIZE, 139, Short.MAX_VALUE)
+                                .addComponent(campo_apellido))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addComponent(boton_saluda)
+                        .addGap(90, 90, 90))))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(48, Short.MAX_VALUE)
-                .addComponent(jLabel1)
+                .addContainerGap()
+                .addComponent(icono_mano)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(etiqueta_instruccion)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(campo_nombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(campo_apellido, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(campo_texto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
-                .addGap(18, 18, 18))
+                .addComponent(boton_saluda)
+                .addContainerGap(26, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -101,20 +122,41 @@ public class practica2_2 extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void boton_saludaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_boton_saludaActionPerformed
         // Obtener el texto ingresado en el campo de texto
-        String nombre = campo_texto.getText();
-
-        // Si no está vacío, muestra el saludo
-        if (!nombre.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "¡Hola, " + nombre + "!", "Saludo", JOptionPane.INFORMATION_MESSAGE);
+        String nombre = campo_nombre.getText();
+        String apellido = campo_apellido.getText();
+        
+        if (nombre.isEmpty() && apellido.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Por favor, introduzca un nombre y un apellido.", "Advertencia", JOptionPane.WARNING_MESSAGE);
+        } else if (nombre.length() < 5) {
+            JOptionPane.showMessageDialog(this, "Por favor, introduzca un nombre con 5 caracteres como minimo.", "Advertencia", JOptionPane.WARNING_MESSAGE);
+        } else if (nombre.matches(".*[0-9].*")) {
+            JOptionPane.showMessageDialog(this, "Por favor, no introduzca caracteres numericos.", "Advertencia", JOptionPane.WARNING_MESSAGE);
         } else {
-            // Si está vacío, mostrar advertencia
-            JOptionPane.showMessageDialog(this, "Por favor, introduce un nombre.", "Advertencia", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(this, "¡Hola, " + nombre + " " + apellido + "!", "Saludo", JOptionPane.INFORMATION_MESSAGE);
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+        campo_nombre.setText("Introduzca nombre");
+        campo_apellido.setText("Introduzca apellido");
+
+    }//GEN-LAST:event_boton_saludaActionPerformed
+
+    private void campo_nombreKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_campo_nombreKeyPressed
+        // TODO add your handling code here:
+        if (campo_nombre.getText().equals("Introduzca nombre")) {
+            campo_nombre.setText("");
+        }
+    }//GEN-LAST:event_campo_nombreKeyPressed
+
+    private void campo_apellidoKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_campo_apellidoKeyPressed
+        // TODO add your handling code here:
+        if (campo_apellido.getText().equals("Introduzca apellido")) {
+            campo_apellido.setText("");
+        }
+    }//GEN-LAST:event_campo_apellidoKeyPressed
 
     /**
      * @param args the command line arguments
@@ -152,10 +194,11 @@ public class practica2_2 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField campo_texto;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JButton boton_saluda;
+    private javax.swing.JTextField campo_apellido;
+    private javax.swing.JTextField campo_nombre;
+    private javax.swing.JLabel etiqueta_instruccion;
+    private javax.swing.JLabel icono_mano;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
